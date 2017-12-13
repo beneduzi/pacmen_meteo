91d5e8e36c0b052defa5949a24549bc6da19e06a
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-12-13 15:43:35 -0200

    Mudancas para se adequar a pep8 Novas funcoes para busca e metodos de acesso a tabelas  Changes to be committed: 	modified:   db_rendler.py 	modified:   models.py 	modified:   template/contato.html 	modified:   template/footer.html 	modified:   template/header.html 	modified:   views.py

--- db_rendler.py
+++ db_rendler.py
@@ -6,83 +6,83 @@ import MySQLdb
 #######################################
 ##Buscas na tabela final
 def _pull_model_data(tipo='*', nome='*', membro='*', lat, lon):
-	try:
-		db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
-		cursor = db.cursor()
-		query = 'SELECT f_id, f_path FROM dados_tb WHERE tipo=%s AND nome=%s AND membro=%s' %(tipo, nome, membro)
-		cursor.execute(query)
-		output = cursor.fetchall()
-		# db.commit()
-		db.close()
-		return (True, output)
-	except:
-		return (False, "Null")
+    try:
+        db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
+        cursor = db.cursor()
+        query = 'SELECT f_id, f_path FROM dados_tb WHERE tipo=%s AND nome=%s AND membro=%s' %(tipo, nome, membro)
+        cursor.execute(query)
+        output = cursor.fetchall()
+        # db.commit()
+        db.close()
+        return (True, output)
+    except:
+        return (False, "Null")
 
 def _pull_date_data(_start='*', _end='*', lat, lon):
-	try:
-		db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
-		cursor = db.cursor()
-		query = 'SELECT f_id, f_path FROM dados_tb WHERE s_date=%s AND e_date=%s '%(_start, _end)
-		cursor.execute(query)
-		output = cursor.fetchall()
-		# db.commit()
-		db.close()
-		return (True, output)
-	except:
-		return (False, "Null")
+    try:
+        db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
+        cursor = db.cursor()
+        query = 'SELECT f_id, f_path FROM dados_tb WHERE s_date=%s AND e_date=%s '%(_start, _end)
+        cursor.execute(query)
+        output = cursor.fetchall()
+        # db.commit()
+        db.close()
+        return (True, output)
+    except:
+        return (False, "Null")
 
 #######################################
 ##Buscas nas tabelas primarias
 def _get_modelos():
-	try:
-		db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
-		cursor = db.cursor()
-		query = 'SELECT name, pt_desc FROM modelos_tb'
-		cursor.execute(query)
-		output = cursor.fetchall()
-		# db.commit()
-		db.close()
-		return (True, output)
-	except:
-		return (False, "Null")
+    try:
+        db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
+        cursor = db.cursor()
+        query = 'SELECT name, pt_desc FROM modelos_tb'
+        cursor.execute(query)
+        output = cursor.fetchall()
+        # db.commit()
+        db.close()
+        return (True, output)
+    except:
+        return (False, "Null")
 
 def _get_periodos():
-	try:
-		db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
-		cursor = db.cursor()
-		query = 'SELECT name, pt_desc FROM periodos_tb'
-		cursor.execute(query)
-		output = cursor.fetchall()
-		# db.commit()
-		db.close()
-		return (True, output)
-	except:
-		return (False, "Null")
+    try:
+        db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
+        cursor = db.cursor()
+        query = 'SELECT name, pt_desc FROM periodos_tb'
+        cursor.execute(query)
+        output = cursor.fetchall()
+        # db.commit()
+        db.close()
+        return (True, output)
+    except:
+        return (False, "Null")
 
 def _get_membros():
-	try:
-		db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
-		cursor = db.cursor()
-		query = 'SELECT name, pt_desc FROM membros_tb'
-		cursor.execute(query)
-		output = cursor.fetchall()
-		# db.commit()
-		db.close()
-		return (True, output)
-	except:
-		return (False, "Null")
+    try:
+        db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
+        cursor = db.cursor()
+        query = 'SELECT name, pt_desc FROM membros_tb'
+        cursor.execute(query)
+        output = cursor.fetchall()
+        # db.commit()
+        db.close()
+        return (True, output)
+    except:
+        return (False, "Null")
 
-def _get_cenarios():
-	try:
-		db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
-		cursor = db.cursor()
-		query = 'SELECT name, pt_desc FROM cenarios_tb'
-		cursor.execute(query)
-		output = cursor.fetchall()
-		# db.commit()
-		db.close()
-		return (True, output)
-	except:
-		return (False, "Null")
+def _get_projetos():
+    try:
+        db = MySQLdb.connect("localhost","root","&i1hm","pacmen_db" )
+        cursor = db.cursor()
+        query = 'SELECT name, pt_desc FROM projetos_tb'
+        cursor.execute(query)
+        output = cursor.fetchall()
+        # db.commit()
+        db.close()
+        return (True, output)
+    except:
+        return (False, "Null")
 
 #######################################
--- views.py
+++ views.py
@@ -16,128 +16,128 @@ import model
 ## Paginas estaticas
 @app.route('/')
 def render_index():
-	"""
-	Renderiza o index, quando o endpoint for o diretorio raiz
-	"""
-	out = datetime.datetime.now()
-	return render_template('index.html', out=out)
+    """
+    Renderiza o index, quando o endpoint for o diretorio raiz
+    """
+    out = datetime.datetime.now()
+    return render_template('index.html', out=out)
 
 @app.route("/hist")
 def render_historia():
-	"""
-	renderiza a pagina de historia do projeto
-	"""
-	return render_template('historia.html')
+    """
+    renderiza a pagina de historia do projeto
+    """
+    return render_template('historia.html')
 
 @app.route("/desc")
 def render_descricao():
-	"""
-	renderiza a descricao dos elementos
-	"""
-	return render_template('descricao.html')
+    """
+    renderiza a descricao dos elementos
+    """
+    return render_template('descricao.html')
 
 @app.route("/cont")
 def render_contato():
-	"""
-	renderiza o form de contato
-	"""
-	return render_template('contato.html')
+    """
+    renderiza o form de contato
+    """
+    return render_template('contato.html')
 
 @app.route("/busca/model")
 def search_model():
-	"""
-	renderiza os forms de busca por modelo
-	"""
-	return render_template('busca_modelo.html')
+    """
+    renderiza os forms de busca por modelo
+    """
+    return render_template('busca_modelo.html')
 
 @app.route("/busca/data")
 def search_data():
-	"""
-	renderiza os forms de busca por data
-	"""
-	return render_template('busca_data.html')
+    """
+    renderiza os forms de busca por data
+    """
+    return render_template('busca_data.html')
 
 #######################################
 ## Forms
 @app.route("/form/cont" , methods=['GET', 'POST'])
 def recv_contato
-	"""
-	endpoint para o form de contato 
-	"""
-	select = request.form.get_dict('FORM___NAME____')  #recebe os dados do form, precisa de nome e valor no html
-	out = parse_form.contato(select)
-	success = model.contato(out[])
-	if success == True:
-		return render_template('email_sended.html')
-	else:
-		return render_template('email_fail.html')
+    """
+    endpoint para o form de contato 
+    """
+    select = request.form.get_dict('FORM___NAME____')  #recebe os dados do form, precisa de nome e valor no html
+    out = parse_form.contato(select)
+    success = model.contato(out[])
+    if success == True:
+        return render_template('email_sended.html')
+    else:
+        return render_template('email_fail.html')
 
 @app.route("/form/model" , methods=['GET', 'POST'])
 def recv_modelo():
-	"""
-	endpoint para o form de busca por modelo
-	"""
-	select = request.form.get_dict('comp_select') #recebe os dados do select
-	out = parse_form.modelo(select)
-	success, f_path, f_id = model.modelo(out[])
-	if success == True:
-		return render_template('file_list.html', _path_=f_path, _id_=f_id) 
-	else:
-		return render_template('file_fail.html')
+    """
+    endpoint para o form de busca por modelo
+    """
+    select = request.form.get_dict('comp_select') #recebe os dados do select
+    out = parse_form.modelo(select)
+    success, out = model.modelo(out[])
+    if success == True:
+        return render_template('file_list.html', _path_=out.path, _id_=out.id, _nome_=out.nome) 
+    else:
+        return render_template('file_fail.html')
 
 @app.route("/form/data", methods=['GET', 'POST']):
 def recv_data():
-	"""
-	endpoint para o form de busca por data
-	"""
-	select = request.form.get_dict('comp_select') #recebe os dados do select
-	out = parse_form.data(select)
-	success, f_path, f_id = model.data(out[])
-	if success == True:
-		return render_template('file_list.html', _path_=f_path, _id_=f_id) 
-	else:
-		return render_template('file_fail.html')
+    """
+    endpoint para o form de busca por data
+    """
+    select = request.form.get_dict('comp_select') #recebe os dados do select
+    out = parse_form.data(select)
+    success, out = model.data(out[])
+    if success == True:
+        return render_template('file_list.html', _path_=out.path, _id_=out.id, _nome_=out.nome) 
+    else:
+        return render_template('file_fail.html')
 #######################################
 ## Descricoes dos elementos
 @app.route("/desc/model")
 def des_modelos():
-	success, out = model.descricao("modelo")
-	if success == True:
-		return render_template('desc_element.html', out=out)
-	else:
-		return render_template('desc_fail.html')
+    success, out = model.descricao("modelo")
+    if success == True:
+        return render_template('desc_element.html', out=out)
+    else:
+        return render_template('desc_fail.html')
 
-@app.route("/desc/cenario")
-def des_cenarios():
-	success, out = model.descricao("cenarios")
-	if success == True:
-		return render_template('desc_element.html', out=out)
-	else:
-		return render_template('desc_fail.html')
+@app.route("/desc/projetos")
+def des_projetos():
+    success, out = model.descricao("projetos")
+    if success == True:
+        return render_template('desc_element.html', out=out)
+    else:
+        return render_template('desc_fail.html')
 
 @app.route("/desc/membros")
 def desc_membros():
-	success, out = model.descricao("membros")
-	if success == True:
-		return render_template('desc_element.html', out=out)
-	else:
-		return render_template('desc_fail.html')
+    success, out = model.descricao("membros")
+    if success == True:
+        return render_template('desc_element.html', out=out)
+    else:
+        return render_template('desc_fail.html')
 
 @app.route("/desc/periodos")
 def desc_periodos():
-	success, out = model.descricao("periodo")
-	if success == True:
-		return render_template('desc_element.html', out=out)
-	else:
-		return render_template('desc_fail.html')
+    success, out = model.descricao("periodo")
+    if success == True:
+        return render_template('desc_element.html', out=out)
+    else:
+        return render_template('desc_fail.html')
 
 @app.route("/desc/all")
 def desc_all():
-	success, out = model.descricao()
-	if success == True:
-		return render_template('desc_element.html', out=out)
-	else:
-		return render_template('desc_fail.html')
+    success, out = model.descricao()
+    if success == True:
+        return render_template('desc_element.html', out=out)
+    else:
+        return render_template('desc_fail.html')
 
 
 # @app.route('/form/login', methods=['GET', 'POST'])
@@ -152,4 +152,4 @@ def desc_all():
 # #######################################
 # ## Execucao
 # if __name__=='__main__':
-# 	app.run(debug=True)
+#   app.run(debug=True)
