898e7ca1dee96cbcfc79abef62f3683103fce976
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2018-09-12 10:57:37 -0300

    Cahngig to port 5000, for tests

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ def opendap():
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
-        port=80,
+        port=8080,
         debug=False,
         processes=4,
         threaded=False,
